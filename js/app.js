5b165c1d0669db85f4ad82892432f94d15490b44
Irina <fransik@bk.ru>
2023-07-04 00:09:51 +0300

    JavaScript hw3

---
+++ js/app.js
@@ -0,0 +1,26 @@
+function app() {
+    let name = document.getElementById("username").value
+    document.getElementById("result").innerText = "Приветствуем Вас, " + name + "!" ;
+    document.getElementById("username").value = '';
+    console.log("Приветствуем Вас, " + name + "!")
+}
+
+// function app1() {
+//     let text = document.getElementById("massage").value
+//     document.getElementById("result1").innerHTML = name + ":" + massage;
+//     document.getElementById("massage").value = '';
+//     console.log(massage)
+// }
+
+
+
+
+// var name = prompt ("Как вас зовут?")
+//     alert ("Привет " + name)
+
+// let age = prompt ("Сколько вам лет?")
+// age = parseInt(age)
+// console.log(age)
+
+// let str = 'Привет, мир';
+// console.log(str);
